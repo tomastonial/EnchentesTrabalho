2e9025698c879e0da30e1e75c9aff0f74cac51c1
cassiano-richato <cassianorichato@cesurg.com>
2025-08-04 20:41:49 -0300

    Commit inicial do projeto EnchentesTrabalho

---
+++ src/main/java/com/cesurg/trabalho_enchente/TrabalhoEnchenteApplication.java
@@ -0,0 +1,13 @@
+package com.cesurg.trabalho_enchente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoEnchenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoEnchenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesurg/trabalho_enchente/TrabalhoEnchenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesurg.trabalho_enchente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoEnchenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
